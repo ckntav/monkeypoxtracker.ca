3f58e169e21c1ecf34aa1dd0f54222491c23a543
Christophe Tav <christophe.tav@gmail.com>
2023-03-03 14:27:35 -0500

    update

--- _codes/_update_mkpt.R
+++ _codes/_update_mkpt.R
@@ -11,7 +11,7 @@ library(rvest)
 # # library(leaflet)
 # library(sf)
 
-date_mkp <- "20230217"
+date_mkp <- "20230303"
 file_mkp <- FALSE
 
 if (TRUE) {
@@ -34,3 +34,4 @@ if (file_mkp) {
   source("_codes/scripts/var_nb_cases_byProvince.R")
   source("_codes/scripts/widget_nb_cases_canada.R")
 }
+
